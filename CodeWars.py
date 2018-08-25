5d67f32e5a674c412bd2332aee02d54468a42130
elc <kirillver359@gmail.com>
2018-08-25 07:47:41 +0300

    updt

--- CodeWars.py
+++ CodeWars.py
@@ -1,20 +1,20 @@
+###########################################################
+# 1. Sum of all the multiples of 3 or 5
+###########################################################
 
-1. Sum of all the multiples of 3 or 5
+## var.1
 
+# def find(n):
+#   l = []
+#   nm = 1
+#   while nm != n+1:
+#       l.append(nm)
+#       nm+=1
+#   l = sum(list(filter(lambda x:x%3 == 0 or x%5 == 0,l)))
 
-# var.1
+#   return l
 
-def find(n):
-  l = []
-  nm = 1
-  while nm != n+1:
-      l.append(nm)
-      nm+=1
-  l = sum(list(filter(lambda x:x%3 == 0 or x%5 == 0,l)))
+# # var.2
 
-  return l
-
-# var.2
-
-def find(n):
-    return sum(e for e in range(1, n+1) if e % 3 == 0 or e % 5 == 0)
+# def find(n):
+#     return sum(e for e in range(1, n+1) if e % 3 == 0 or e % 5 == 0)
